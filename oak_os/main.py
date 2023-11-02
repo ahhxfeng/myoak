9e9e20b20a57f9017fa97d757750d6066205e1bb
TF <ahhxfeng@gmail.com>
2023-11-02 16:42:59 +0800

    update sign

---
+++ oak_os/main.py
@@ -0,0 +1,15 @@
+#coding=utf-8
+
+import argparse
+import time
+
+def main():
+    parser = argparse.ArgumentParser(description="process some interger about the miner os program")
+    parser.add_argument("--command_interval", type=int, default=5, help="interval between command loop resquest(second)")
+    parser.add_argument("--report_interval", type=int, default=15, help="interval between report requests( second )")
+    parser.add_argument("--log_scan_interval", type=int, default=30, help="interval between log scan and upload requests (second)")
+    parser.add_argument("--miner_interval", type=int, default=10, help="interval between miner requests (second)")
+    args = parser.parse_args()
+
+    time.sleep(5)
+
