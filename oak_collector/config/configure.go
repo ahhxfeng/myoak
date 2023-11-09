1256e945e532c487d04b2202988dbbb560fc5344
TF <ahhxfeng@gmail.com>
2023-11-09 19:32:32 +0800

    fix warn about tag

--- oak_collector/config/configure.go
+++ oak_collector/config/configure.go
@@ -50,42 +50,42 @@ import (
 type GlobalConfig struct {
 	// listen server
 	PublicAddress  string `json:"public_address"`
-	PrivateAddress string `json: "private_address"`
+	PrivateAddress string `json:"private_address"`
 
 	// sign pipe
-	SignPipePrivateKeyPath  string   `json: "private_key_path"`
-	AvailableCommand        []string `json: "availabel_command"`
-	LongPoolingTime         int      `json: "long_pooling_time"`
-	LongPoolingNotifyBucket int      `json: "long_pooling_notify_bucket"`
-	UploadDir               string   `json: "upload_dir"`
-	UploadLimmit            int      `json: "upload_limmit"`
+	SignPipePrivateKeyPath  string   `json:"private_key_path"`
+	AvailableCommand        []string `json:"availabel_command"`
+	LongPoolingTime         int      `json:"long_pooling_time"`
+	LongPoolingNotifyBucket int      `json:"long_pooling_notify_bucket"`
+	UploadDir               string   `json:"upload_dir"`
+	UploadLimmit            int      `json:"upload_limmit"`
 
 	//version
-	VersionName string `json: "version_name"`
-	VersionUrl  string `json: "version_url"`
-	VersionMd5  string `json: "version_md5"`
+	VersionName string `json:"version_name"`
+	VersionUrl  string `json:"version_url"`
+	VersionMd5  string `json:"version_md5"`
 
 	// db
-	DatabaseDsn              string `json: "database_url"`
-	DatabaseDriver           string `json: "database_driver"`
-	DatabaseDownloadInterval int    `json: "database_download_interval"`
-	DatabaseUploadInterval   int    `json: "database_upload_interval"`
+	DatabaseDsn              string `json:"database_url"`
+	DatabaseDriver           string `json:"database_driver"`
+	DatabaseDownloadInterval int    `json:"database_download_interval"`
+	DatabaseUploadInterval   int    `json:"database_upload_interval"`
 
 	// Redis
-	RedisAddress     string `json: "redis_url"`
-	RedisMaxIdle     int    `defualt: "redis_max_idle"`
-	RedisIdleTimeout int    `json: "redis_idle_timeout"`
-	RedisRigTtl      int    `json: "redis_rig_ttl"` // rig信息保存多少秒
-	RedisCmdTtl      int    `json: redis_cmd_ttl`   //Cmd 信息保存多少秒
+	RedisAddress     string `json:"redis_url"`
+	RedisMaxIdle     int    `defualt:"redis_max_idle"`
+	RedisIdleTimeout int    `json:"redis_idle_timeout"`
+	RedisRigTtl      int    `json:"redis_rig_ttl"` // rig信息保存多少秒
+	RedisCmdTtl      int    `json:"redis_cmd_ttl"` //Cmd 信息保存多少秒
 	// log
-	LogDir      string `json: "log_dir"`
-	LogLevel    string `json: "log_level"`
-	WinLogLevel string `json: "win_log_level"`
+	LogDir      string `json:"log_dir"`
+	LogLevel    string `json:"log_level"`
+	WinLogLevel string `json:"win_log_level"`
 
 	//statsd
 
-	StatsdAddress string `json: "statsdURl"`
-	StatsdPrefix  string `json: "statsd_prefix"`
+	StatsdAddress string `json:"statsdURl"`
+	StatsdPrefix  string `json:"statsd_prefix"`
 }
 
 func NewConfig() *GlobalConfig {
